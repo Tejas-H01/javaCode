28af5465fc32db6ee0ac86ef0049e77dabf49dd6
Tejas Halvankar <tejashalvankar0@gmail.com>
2025-12-29 17:31:24 +0530

    Array Reverse

---
+++ Array/Reverse.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Reverse {
+    public static void main(String[] args) {
+        char arr[]={'H','e','l','l','o'};
+        System.out.println("------------------------------------");
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
