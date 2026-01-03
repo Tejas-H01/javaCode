cb39f0ac94404ff43a638db1dc9d6389644c278a
Tejas Halvankar <tejashalvankar0@gmail.com>
2026-01-03 23:54:47 +0530

    OOP code example file1(Employee) added

---
+++ OOP_codes/Inheritance/Employee.java
@@ -0,0 +1,10 @@
+package OOP_codes.Inheritance;
+
+public class Employee {
+    String EmployeeName;
+    int basicSalary = 50000;
+
+    public int calculateSalary(){
+        return basicSalary;
+    }
+}
