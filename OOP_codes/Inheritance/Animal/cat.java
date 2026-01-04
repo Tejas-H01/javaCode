f177daf7f7fe45e90b31a6884c0cfb1a548fc004
Tejas Halvankar <tejashalvankar0@gmail.com>
2026-01-05 01:23:29 +0530

    Inheritance Employee

--- OOP_codes/Inheritance/Animal/cat.java
+++
@@ -1,7 +0,0 @@
-package OOP_codes.Inheritance.Animal;
-
-public class cat extends animal{
-    public void sayHello(){
-        System.out.println("meow meow");
-    }
-}
--- OOP_codes/Inheritance/Animal/dog.java
+++
@@ -1,9 +0,0 @@
-package OOP_codes.Inheritance.Animal;
-
-public class dog extends animal{
-
-    @Override
-    public void sayHello(){
-        System.out.println("bowww boww");
-    }
-}
